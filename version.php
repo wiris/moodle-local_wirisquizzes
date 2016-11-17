c7648d92ad8b5eb62ff231f1bda5d21f9538bf80
Manuel Cagigas <manuel@wiris.com>
2016-11-17 10:07:12 +0100

    WIRIS Quizzes local plugin

---
+++ version.php
@@ -0,0 +1,40 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version details for WIIRIS Quizzes local plugin.
+ * This plugin declares all the depeecies on all WIRIS Question types.
+ *
+ * @package    local
+ * @subpackage wirisquizzes
+ * @copyright  Maths for More S.L. <info@wiris.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = 2016111600;
+$plugin->requires = 2011060313;
+$plugin->component = 'local_wirisquizzes';
+$plugin->dependencies = array(
+    'qtype_wq' => 2016111600,
+    'qtype_essaywiris' => 2016111600,
+    'qtype_matchwiris' => 2016111600,
+    'qtype_multianswerwiris' => 2016111600,
+    'qtype_multichoicewiris' => 2016111600,
+    'qtype_shortanswerwiris' => 2016111600,
+    'qtype_truefalsewiris' => 2016111600
+);
