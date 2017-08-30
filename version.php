31beec4509c0e4770d54b448dbc433a88fbe8036
Manuel <manuel@wiris.com>
2017-08-30 08:13:40 +0200

    2017082900 Stable version

--- version.php
+++ version.php
@@ -27,9 +27,9 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2017082900;
-
+$plugin->release = '3.55.0.1025';
 $plugin->requires = 2011060313;
-$plugin->maturity = MATURITY_BETA;
+$plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'local_wirisquizzes';
 $plugin->dependencies = array(
     'qtype_wq' => 2017082900,
