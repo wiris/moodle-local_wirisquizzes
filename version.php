bacd670db56a15d3f1530a0c1a348980f23971e1
Manuel <manuel@wiris.com>
2017-12-12 23:06:52 +0100

    2017121200 development version

--- version.php
+++ version.php
@@ -27,9 +27,9 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2017121200;
-$plugin->release = '3.59.0.1031';
+
 $plugin->requires = 2011060313;
-$plugin->maturity = MATURITY_STABLE;
+$plugin->maturity = MATURITY_BETA;
 $plugin->component = 'local_wirisquizzes';
 $plugin->dependencies = array(
     'qtype_wq' => 2017121200,
