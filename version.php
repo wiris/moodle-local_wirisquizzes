4a013c559729d7927c5e4b3607088e45b942fe3a
Manuel <manuel@wiris.com>
2017-12-20 23:11:50 +0100

    2017122000 development version

--- version.php
+++ version.php
@@ -26,17 +26,17 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2017121900;
+$plugin->version = 2017122000;
 
 $plugin->requires = 2011060313;
 $plugin->maturity = MATURITY_BETA;
 $plugin->component = 'local_wirisquizzes';
 $plugin->dependencies = array(
-    'qtype_wq' => 2017121900,
-    'qtype_essaywiris' => 2017121900,
-    'qtype_matchwiris' => 2017121900,
-    'qtype_multianswerwiris' => 2017121900,
-    'qtype_multichoicewiris' => 2017121900,
-    'qtype_shortanswerwiris' => 2017121900,
-    'qtype_truefalsewiris' => 2017121900
+    'qtype_wq' => 2017122000,
+    'qtype_essaywiris' => 2017122000,
+    'qtype_matchwiris' => 2017122000,
+    'qtype_multianswerwiris' => 2017122000,
+    'qtype_multichoicewiris' => 2017122000,
+    'qtype_shortanswerwiris' => 2017122000,
+    'qtype_truefalsewiris' => 2017122000
 );
