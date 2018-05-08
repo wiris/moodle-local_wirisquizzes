ea7eb494182423b22ebd71ba5b3dd1ad5a21026f
Jenkins <jenkins@wiris.com>
2018-05-08 10:41:19 +0200

    2018050800 Stable version

--- version.php
+++ version.php
@@ -27,9 +27,9 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2018050800;
-
+$plugin->release = '3.64.0.1038';
 $plugin->requires = 2011060313;
-$plugin->maturity = MATURITY_BETA;
+$plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'local_wirisquizzes';
 $plugin->dependencies = array(
     'qtype_wq' => 2018050800,
