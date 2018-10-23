0cb580b693ecfe601d496840106b965799200684
Jenkins <jenkins@wiris.com>
2018-10-23 05:03:41 +0200

    2018102300 development version

--- version.php
+++ version.php
@@ -26,17 +26,17 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2018102200;
+$plugin->version = 2018102300;
 
 $plugin->requires = 2011060313;
 $plugin->maturity = MATURITY_BETA;
 $plugin->component = 'local_wirisquizzes';
 $plugin->dependencies = array(
-    'qtype_wq' => 2018102200,
-    'qtype_essaywiris' => 2018102200,
-    'qtype_matchwiris' => 2018102200,
-    'qtype_multianswerwiris' => 2018102200,
-    'qtype_multichoicewiris' => 2018102200,
-    'qtype_shortanswerwiris' => 2018102200,
-    'qtype_truefalsewiris' => 2018102200
+    'qtype_wq' => 2018102300,
+    'qtype_essaywiris' => 2018102300,
+    'qtype_matchwiris' => 2018102300,
+    'qtype_multianswerwiris' => 2018102300,
+    'qtype_multichoicewiris' => 2018102300,
+    'qtype_shortanswerwiris' => 2018102300,
+    'qtype_truefalsewiris' => 2018102300
 );
