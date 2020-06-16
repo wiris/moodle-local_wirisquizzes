d97f1b1f86e48a5148231f64bac060c33f231f05
Manuel <manuel@wiris>
2020-06-16 08:06:50 +0200

    Release 3.78.2

--- version.php
+++ version.php
@@ -26,17 +26,17 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2020011300;
-$plugin->release = '3.78.0';
+$plugin->version = 2020061600;
+$plugin->release = '3.78.2';
 $plugin->requires = 2011060313;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'local_wirisquizzes';
 $plugin->dependencies = array(
-    'qtype_wq' => 2020011300,
-    'qtype_essaywiris' => 2020011300,
-    'qtype_matchwiris' => 2020011300,
-    'qtype_multianswerwiris' => 2020011300,
-    'qtype_multichoicewiris' => 2020011300,
-    'qtype_shortanswerwiris' => 2020011300,
-    'qtype_truefalsewiris' => 2020011300
+    'qtype_wq' => 2020061600,
+    'qtype_essaywiris' => 2020061600,
+    'qtype_matchwiris' => 2020061600,
+    'qtype_multianswerwiris' => 2020061600,
+    'qtype_multichoicewiris' => 2020061600,
+    'qtype_shortanswerwiris' => 2020061600,
+    'qtype_truefalsewiris' => 2020061600
 );
