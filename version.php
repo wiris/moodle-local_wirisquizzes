c17798da38d4bae97425443ff76fa6a577ac28de
Pol Torrent i Soler <pol@wiris.com>
2020-11-10 16:29:53 +0100

    Version 3.78.3

--- version.php
+++ version.php
@@ -26,17 +26,17 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2020061600;
-$plugin->release = '3.78.2';
+$plugin->version = 2020111000;
+$plugin->release = '3.78.3';
 $plugin->requires = 2011060313;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'local_wirisquizzes';
 $plugin->dependencies = array(
-    'qtype_wq' => 2020061600,
-    'qtype_essaywiris' => 2020061600,
-    'qtype_matchwiris' => 2020061600,
-    'qtype_multianswerwiris' => 2020061600,
-    'qtype_multichoicewiris' => 2020061600,
-    'qtype_shortanswerwiris' => 2020061600,
-    'qtype_truefalsewiris' => 2020061600
+    'qtype_wq' => 2020111000,
+    'qtype_essaywiris' => 2020111000,
+    'qtype_matchwiris' => 2020111000,
+    'qtype_multianswerwiris' => 2020111000,
+    'qtype_multichoicewiris' => 2020111000,
+    'qtype_shortanswerwiris' => 2020111000,
+    'qtype_truefalsewiris' => 2020111000
 );
