4b1d0993a94c864286e7fc95e3a346aa8f084a1d
Pol Torrent i Soler <66125018+ptorrent-at-wiris@users.noreply.github.com>
2021-12-22 13:44:12 +0100

    chore: updates version to 4.4.0

--- version.php
+++ version.php
@@ -26,17 +26,17 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2021111500;
-$plugin->release = '4.3.3';
+$plugin->version = 2021122100;
+$plugin->release = '4.4.0';
 $plugin->requires = 2011060313;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'local_wirisquizzes';
 $plugin->dependencies = array(
-    'qtype_wq' => 2021111500,
-    'qtype_essaywiris' => 2021111500,
-    'qtype_matchwiris' => 2021091300,
-    'qtype_multianswerwiris' => 2021091300,
-    'qtype_multichoicewiris' => 2021091300,
-    'qtype_shortanswerwiris' => 2021091300,
-    'qtype_truefalsewiris' => 2021091500
+    'qtype_wq' => 2021122100,
+    'qtype_essaywiris' => 2021122100,
+    'qtype_matchwiris' => 2021122100,
+    'qtype_multianswerwiris' => 2021122100,
+    'qtype_multichoicewiris' => 2021122100,
+    'qtype_shortanswerwiris' => 2021122100,
+    'qtype_truefalsewiris' => 2021122100
 );
