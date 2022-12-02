f0a1d363b603176d9c2d754326d5fe08c2050071
Pol Torrent i Soler <66125018+ptorrent-at-wiris@users.noreply.github.com>
2022-12-02 11:08:15 +0100

    chore: bump version to 4.6.0

--- version.php
+++ version.php
@@ -26,17 +26,17 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2022111100;
+$plugin->version = 2022120200;
 $plugin->requires = 2015111600; // Moodle 3.0.
-$plugin->release = '4.5.3';
+$plugin->release = '4.6.0';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'local_wirisquizzes';
 $plugin->dependencies = array(
-    'qtype_wq' => 2022111100,
-    'qtype_essaywiris' => 2022111100,
-    'qtype_matchwiris' => 2022111100,
-    'qtype_multianswerwiris' => 2022111100,
-    'qtype_multichoicewiris' => 2022111100,
-    'qtype_shortanswerwiris' => 2022111100,
-    'qtype_truefalsewiris' => 2022111100
+    'qtype_wq' => 2022120200,
+    'qtype_essaywiris' => 2022120200,
+    'qtype_matchwiris' => 2022120200,
+    'qtype_multianswerwiris' => 2022120200,
+    'qtype_multichoicewiris' => 2022120200,
+    'qtype_shortanswerwiris' => 2022120200,
+    'qtype_truefalsewiris' => 2022120200
 );
