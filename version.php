818f40964f23d70628372e4848aa3d43cff68904
Ubaldo Santos <ubaldo.santos@wiris.com>
2026-07-20 16:40:56 +0200

    chore: bump version to v4.18.0

--- version.php
+++ version.php
@@ -26,17 +26,17 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2026060400;
+$plugin->version = 2026072000;
 $plugin->requires = 2024100700; // Moodle 4.5.0.
-$plugin->release = '4.17.0';
+$plugin->release = '4.18.0';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'local_wirisquizzes';
 $plugin->dependencies = array(
-    'qtype_wq' => 2026060400,
-    'qtype_essaywiris' => 2026060400,
-    'qtype_matchwiris' => 2026060400,
-    'qtype_multianswerwiris' => 2026060400,
-    'qtype_multichoicewiris' => 2026060400,
-    'qtype_shortanswerwiris' => 2026060400,
-    'qtype_truefalsewiris' => 2026060400
+    'qtype_wq' => 2026072000,
+    'qtype_essaywiris' => 2026072000,
+    'qtype_matchwiris' => 2026072000,
+    'qtype_multianswerwiris' => 2026072000,
+    'qtype_multichoicewiris' => 2026072000,
+    'qtype_shortanswerwiris' => 2026072000,
+    'qtype_truefalsewiris' => 2026072000
 );
